7c14cd83b7977b1fed3063f0c9b5b0cf463957bc
Jesse Sandvik <sandvikjesse@gmail.com>
2022-02-28 02:15:20 -0500

    fixed padding issue with header

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -17,14 +17,16 @@ const Dashboard = () => {
         </ul>
       </nav>
       <div className="two">
-        <div className="item three"></div>
+        <div className="item three">
+          <h2>Good Evening, Tara!</h2>
+        </div>
         <div className="item four"></div>
         <div className="item five"></div>
         <div className="item six"></div>
         <div className="item seven"></div>
         <div className="item eight"></div>
         <div className="item nine"></div>
-        <div className="item nine"></div>
+        <div className="item ten"></div>
       </div>
     </section>
   );
